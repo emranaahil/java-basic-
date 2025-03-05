fcc05230c680e69bb3cfc397350507c5597ed10d
CODECLOUDS-IMRAN <CODECLOUDS-IMRAN@CISPL-IO-188>
2025-03-05 17:07:50 +0530

    05/03/2025

--- TestJ/src/Testjj/ReadAndWriteDataIntoArray.java
+++ TestJ/src/Testjj/ReadAndWriteDataIntoArray.java
@@ -5,25 +5,25 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class ReadAndWriteDataIntoArray {
-	
+	//Storing Array in different position
 	public static void main(String[] args) {
 		
 	int a[]= new int[5];
 	
 Scanner sc= new Scanner(System.in);
-//	
-//	for(int i=0; i< a.length; i++)
-//	{
-//		System.out.print("Enter a Number"); //ask for value 
-//		a[i]=sc.nextInt();//store the give value 
-//	}
-//	
-//	System.out.print("Array stored");
-//	
-//	System.out.println(Arrays.toString(a));
+	
+	for(int i=0; i< a.length; i++)
+	{
+		System.out.println("Enter a Number for postion "+i+" : "); //ask for value 
+		a[i]=sc.nextInt();//store the give value 
+	}
+	
+	System.out.print("Array stored");
+	
+	System.out.println(Arrays.toString(a));
 //
 
-	//Storing Array in different position
+	
 
 	
 	}
