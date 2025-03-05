01ea6f6a186d0ae20512f022814996b78c457306
CODECLOUDS-IMRAN <CODECLOUDS-IMRAN@CISPL-IO-188>
2025-03-05 16:36:46 +0530

    05/03/2025

---
+++ TestJ/src/Testjj/ReadAndWriteDataIntoArray.java
@@ -0,0 +1,27 @@
+package Testjj;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReadAndWriteDataIntoArray {
+	
+	public static void main(String[] args) {
+		
+	int a[]= new int[5];
+	
+//	Scanner sc= new Scanner(System.in);
+//	
+//	for(int i=0; i< a.length; i++)
+//	{
+//		System.out.print("Entre a Number"); //ask for value 
+//		a[i]=sc.nextInt();//store the give value 
+//	}
+//	
+//	System.out.print("Array stored");
+//	
+//	System.out.println(Arrays.toString(a));
+//
+}
+	
+}
