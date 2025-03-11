5f78b445b9ba8b6f9e84aeb52ffc57d24f322a0b
CODECLOUDS-IMRAN <CODECLOUDS-IMRAN@CISPL-IO-188>
2025-03-11 20:30:01 +0530

    ComparingString

---
+++ TestJ/src/Testjj/ComparingString.java
@@ -0,0 +1,34 @@
+package Testjj;
+
+public class ComparingString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Different for equal and == methods
+		//==  always use to object    
+		//.equals always use to compare values of objects  
+	//With variable
+//		String a="Hello"; // this is a variable 
+//		String b="Hello"; // this is a variable 
+//		System.out.print(a==b);//this will compare object  and return true
+//		System.out.println(a.equals(b));//this will compare the value and return true
+		
+	////With Object 
+//		String c= new String("Hello"); //this is an object
+//		String d= new String("Hello"); // this is an object
+//		System.out.println(c==d);//this will compare object and return false
+//		System.out.println(c.equals(d));//this will compare the object or variable value and return true
+
+		//With object and variable
+//		String a="Hello"; // this is a variable 
+//		String d= new String("Hello");//this is an object
+//		System.out.println(a==d);//this will compare object and return false
+//		System.out.println(a.equals(d));//this will always use to compare values of objects  return true					
+		
+		///With object, variable
+//		
+		
+	}
+
+}
