24102cb49494f9c70ebc44bf7be7a213bfdd4c83
CODECLOUDS-IMRAN <CODECLOUDS-IMRAN@CISPL-IO-188>
2025-03-12 19:21:05 +0530

    o

--- TestJ/src/Class_Objects_Varibles/Employee.java
+++ TestJ/src/Class_Objects_Varibles/Employee.java
@@ -1,9 +1,32 @@
 package Class_Objects_Varibles;
 
 public class Employee {
+	
+	//Variables 
+	int age;
+	int epin;
+	String name;
+	
+	//Methods
+	void display()
+	{
+		System.out.println("Employee name is "+name);
+		System.out.println("Employee epin is "+epin);
+		System.out.println("Employee age is "+age);
+		
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//object
+		
+		Employee emp1=new Employee();
+		emp1.age=20;
+		emp1.epin=2285;
+		emp1.name="Ravi";
+		emp1.display();
 
 	}
 
